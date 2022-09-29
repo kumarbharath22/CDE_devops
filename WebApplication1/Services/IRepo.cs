d9a1ce861499ec3a0b9de648a091615c98106670
kumarbharath22 <bkumar2212001@gmail.com>
2022-09-29 13:56:33 +0530

    first commit

---
+++ WebApplication1/Services/IRepo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Services
+{
+    public interface IRepo<K, T>
+    {
+        public Task<T> Add(T item);
+        public Task<T> Update(T item);
+        public Task<T> GetT(K id);
+        public Task<ICollection<T>> GetAll();
+    }
+}
